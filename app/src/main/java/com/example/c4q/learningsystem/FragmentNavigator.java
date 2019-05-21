3454abe8f69c55c08aacef32e856a7db8f07a59a
Victoria Leon <vcrleon@gmail.com>
2019-05-20 20:09:55 -0400

    created FragmentNavigator interface and refactored Sign In page to a Fragment

---
+++ app/src/main/java/com/example/c4q/learningsystem/FragmentNavigator.java
@@ -0,0 +1,8 @@
+package com.example.c4q.learningsystem;
+
+public interface FragmentNavigator {
+    void signIn();
+    void displayLessons();
+    void displayLessonDetails();
+    void displayNewLessonForm();
+}
--- app/src/main/java/com/example/c4q/learningsystem/MainActivity.java
+++ app/src/main/java/com/example/c4q/learningsystem/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.c4q.learningsystem;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 
 import com.example.c4q.learningsystem.models.Lessons;
 import com.example.c4q.learningsystem.models.User;
+import com.example.c4q.learningsystem.ui.LessonsFragment;
+import com.example.c4q.learningsystem.ui.SignIn;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -25,103 +28,42 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.username)
-    EditText usernameInput;
-    @BindView(R.id.password)
-    EditText passwordInput;
-    @BindView(R.id.login_button)
-    Button loginBttn;
-    @BindView(R.id.signup_button)
-    Button signUpBttn;
-
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference currentDBUser;
-
+public class MainActivity extends AppCompatActivity implements FragmentNavigator{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        currentDBUser = FirebaseDatabase.getInstance().getReference("users");
-
-        signUp();
-        login();
+        signIn();
     }
 
-    public void login() {
-        loginBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userLogin();
-
-            }
-        });
+    @Override
+    public void signIn() {
+        final Fragment signInFragment = SignIn.newInstance();
+        inflateFragment(signInFragment);
     }
 
-    public void userLogin() {
-        final String email = usernameInput.getText().toString();
-        final String password = passwordInput.getText().toString();
-
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Credentials can't be empty", Toast.LENGTH_SHORT).show();
-        } else {
-
-            firebaseAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this, "Please enter valid credentials!", Toast.LENGTH_SHORT).show();
-                            } else if (task.isSuccessful()) {
-
-                                Intent i = new Intent(MainActivity.this, LessonsActivity.class);
-                                startActivity(i);
-
-                            }
-
-                        }
-                    });
-        }
-
+    @Override
+    public void displayLessons() {
+        final Fragment lessonsFragment = LessonsFragment.newInstance();
+        inflateFragment(lessonsFragment);
 
     }
 
-    public void signUp() {
-        signUpBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userSignUp();
-            }
-        });
+    @Override
+    public void displayLessonDetails() {
 
     }
 
-    public void userSignUp(){
-        final String email = usernameInput.getText().toString();
-        final String password = passwordInput.getText().toString();
-        final List<Lessons> lessons = new ArrayList<>();
-
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Error in Signing Up", Toast.LENGTH_SHORT).show();
-                        } else if (task.isSuccessful()) {
-                            String userId = firebaseAuth.getCurrentUser().getUid();
-                            User currentUser = new User(email, password, userId, lessons);
-                            currentDBUser.child(userId).setValue(currentUser);
-                            Toast.makeText(MainActivity.this, "You've signed up. Now you can log in!", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
+    @Override
+    public void displayNewLessonForm() {
     }
 
-
+    public void inflateFragment(@NonNull Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
 }
